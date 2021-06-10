19f6e53775e6ce569670492f24d080c555525146
Paolo Zanotti <zanotti.paolo2001@gmail.com>
2021-06-10 16:26:33 +0200

    Fixed 08/06/2021

--- Prove_Esame/B/prima_giugno2021.java
+++ Prove_Esame/B/prima_giugno2021.java
@@ -74,22 +74,8 @@ public class prima_giugno2021 {
 		int[] costi = {10, 0, 100, 5, 9, 10, 200, 50};
 		AlberoItem albero = new AlberoItem();
 		
-		Item i1 = new Item(costi[0]);
-		albero.insert(i1);
-		Item i2 = new Item(costi[1]);
-		albero.insert(i2);
-		Item i3 = new Item(costi[2]);
-		albero.insert(i3);
-		Item i4 = new Item(costi[3]);
-		albero.insert(i4);
-		Item i5 = new Item(costi[4]);
-		albero.insert(i5);
-		Item i6 = new Item(costi[5]);
-		albero.insert(i6);
-		Item i7 = new Item(costi[6]);
-		albero.insert(i7);
-		Item i8 = new Item(costi[7]);
-		albero.insert(i8);
+		for(int i=0; i<costi.length; i++)
+			albero.insert(new Item(costi[i]));
 		
 		albero.print();
 		
